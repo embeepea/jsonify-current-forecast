ca83f9d2a046034c72083b4b747523389ace13d9
dbetke <danielle.betke@gmail.com>
2013-05-09 14:35:25 -0400

    updates currentToString method to properly handle multiple months

--- src/currentToString.js
+++ src/currentToString.js
@@ -1,13 +1,13 @@
 function getKeys(obj) {
     'use strict';
     var keys = [];
-
+ 
     for (var k in obj) {
-        if (!obj.hasOwnProperty(k)){ 
-            continue;
-        }
+        
         keys.push(k);
+
     }
+
     return keys;
 }
 
@@ -36,7 +36,9 @@ function currentWeatherXMLObjectToStringArray(obj, parameters) {
         tempObj,
         tempArr,
         newObj = {},
-        fullDate;
+        fullDate,
+        tempObjByYear,
+        tempObjByMonth;
 
     for (count = 0; count < parameters.length; count += 1) {
         paramObj = parameters[count];
@@ -54,47 +56,47 @@ function currentWeatherXMLObjectToStringArray(obj, parameters) {
         }
 
         years = getKeys(tempObj);
-        
+
         for (var i = 0; i < years.length; i += 1) {
             year = years[i];
-            tempObj = tempObj[year];
+            tempObjByYear = tempObj[year];
 
-            months = getKeys(tempObj);
-   
+            months = getKeys(tempObjByYear);
+
             for (var j = 0; j < months.length; j += 1) {
-                month = months[j];
-                tempObj = tempObj[month];
+               month = months[j];
+               tempObjByMonth = tempObjByYear[month];
 
-                days = getKeys(tempObj);
+               days = getKeys(tempObjByMonth);
 
-                for (var k = 0; k < days.length; k += 1) {
-                    day = days[k];
-                    tempArr = tempObj[day];         
-                    for (var l = 0; l < tempArr.length; l += 1) {
-                        if (tempArr[l] !== undefined) {
-                            hour = pad(l);
-                            calendarMonth = Number(month) + 1;
-                            value = String(tempArr[l]);
-                            calendarMonth = pad(Number(month) + 1);
-                            calendarDay = pad(Number(day) + 1);
+               for (var k = 0; k < days.length; k += 1) {
+                   day = days[k];
+                   tempArr = tempObjByMonth[day]; 
+                           
+                   for (var m = 0; m < tempArr.length; m += 1) {
+                       if (tempArr[m] !== undefined) {
+                           hour = pad(m);
+                           calendarMonth = Number(month) + 1;
+                           value = String(tempArr[m]);
+                           calendarMonth = pad(Number(month) + 1);
+                           calendarDay = pad(Number(day) + 1);
 
-                            fullDate = year + calendarMonth + calendarDay + hour;
-                            if (!newObj[fullDate]) {
-                                newObj[fullDate] = [];
-                            }
-                            
-                            if (newObj[fullDate][0] !== fullDate) { 
-                                newObj[fullDate].push(fullDate, value);
-                            } else {
-                                newObj[fullDate].push(value);
-                            }
-                        }
+                           fullDate = year + calendarMonth + calendarDay + hour;
+                           if (!newObj[fullDate]) {
+                               newObj[fullDate] = [];
+                           }
+                           
+                           if (newObj[fullDate][0] !== fullDate) { 
+                               newObj[fullDate].push(fullDate, value);
+                           } else {
+                               newObj[fullDate].push(value);
+                           }
+                       }
                     }
                 }
             }
         }
     }
-
 
     //For every key (date) in newObj, push the array into arrayOfStrings.  
     for (var d in newObj) {
