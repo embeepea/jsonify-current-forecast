f0f09222c446a009a59825e30225e45699198581
dbetke <danielle.betke@gmail.com>
2013-05-09 14:30:06 -0400

    adds tests from second data file

--- spec/currentToString.js
+++ spec/currentToString.js
@@ -47,7 +47,6 @@ describe("currentWeatherXMLObjectToStringArray", function () {
             });
 
             it('should store arrays in the array ordered by date and time', function () {
-                console.log(arr);
                 expect(arr[0][0]).toEqual('2013041112');
                 expect(arr[10][0]).toEqual('2013041122');
                 expect(arr[15][0]).toEqual('2013041203');
@@ -86,4 +85,80 @@ describe("currentWeatherXMLObjectToStringArray", function () {
 
         });
     });
+
+    describe("parsing from test file data/Seattle_0430-0507.xml", function () {
+
+        var loadingFinished = false,
+            data,
+            n,
+            arr;
+
+        $.ajax({
+            url : '../data/Seattle_0430-0507.xml',
+            dataType : 'text',
+            success : function (response) {
+                loadingFinished = true;
+                data = response;
+            }
+        });
+
+        beforeEach(function () {
+            
+            waitsFor(function() {
+                if (loadingFinished) {
+                    n = handleXML(data);
+                    arr = currentWeatherXMLObjectToStringArray(n, [ { 'parameter' : 'temperature', 'type' : 'hourly' },
+                                                                    { 'parameter' : 'temperature', 'type' : 'dew point' },
+                                                                    { 'parameter' : 'probability-of-precipitation', 'type' : 'floating' } ]);
+                }
+
+                return loadingFinished;
+
+            });
+        });
+
+        describe('currentWeatherXMLObjectToStringArray', function () {
+         
+            it('should return an array', function () {
+                
+                console.log(n);
+                
+                expect(arr).toEqual(jasmine.any(Array));
+            });
+
+            it('should store arrays in the array ordered by date and time', function () {
+                expect(arr[0][0]).toEqual('2013043010');
+                expect(arr[10][0]).toEqual('2013043020');
+                expect(arr[15][0]).toEqual('2013050101');
+                expect(arr[35][0]).toEqual('2013050121');
+                expect(arr[60][0]).toEqual('2013050222'); 
+            });
+
+            it('should store the values of the parameters called as a string', function () {
+                //hourly temperatures
+                expect(arr[0][1]).toEqual('42');
+                expect(arr[10][1]).toEqual('55');
+                expect(arr[15][1]).toEqual('45');
+                expect(arr[35][1]).toEqual('58');
+                expect(arr[60][1]).toEqual('60');
+
+                //dew point temperature
+                expect(arr[0][2]).toEqual('33');
+                expect(arr[10][2]).toEqual('34');
+                expect(arr[15][2]).toEqual('35');
+                expect(arr[35][2]).toEqual('35');
+                expect(arr[60][2]).toEqual('39');
+
+                //probablility of precipitation
+                expect(arr[0][3]).toEqual('15');
+                expect(arr[10][3]).toEqual('3');
+                expect(arr[15][3]).toEqual('3');
+                expect(arr[35][3]).toEqual('0');
+                expect(arr[60][3]).toEqual('0');
+            });
+
+        });
+    });
+
+
 });
